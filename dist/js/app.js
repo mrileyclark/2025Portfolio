a1362e5a8cd96b3bcd3c7fd5daee9bb2fbd1b31e
mclark <maryariley4578@yahoo.com>
2025-07-27 14:51:19 -0400

    update animation on hero section

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,17 +15,17 @@ function clearInputField() {
 }
 
 //animates on hero section only
-if (window.scrollY < 50) {
-  codeIntro.classList.add('animated');
-  imageIntro.classList.add('animated');
-}
+// if (window.scrollY < 50) {
+//   codeIntro.classList.add('animated');
+//   imageIntro.classList.add('animated');
+// }
 
-// window.addEventListener("load", function () {
-//   if (window.scrollY === 0) {
-//     const codeIntro = document.querySelector(".code-intro");
-//     const imageIntro = document.querySelector(".image-intro");
+window.addEventListener("load", function () {
+  if (window.scrollY === 0) {
+    const codeIntro = document.querySelector(".code-intro");
+    const imageIntro = document.querySelector(".image-intro");
 
-//     codeIntro?.classList.add("slide-left");
-//     imageIntro?.classList.add("slide-right");
-//   }
-// });
+    codeIntro?.classList.add("slide-left");
+    imageIntro?.classList.add("slide-right");
+  }
+});
